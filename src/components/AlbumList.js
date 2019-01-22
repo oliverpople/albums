aafedf5c5a8d60e4e1bbc8431ce206a0c7da1d54
oliverpople <oliverpople@gmail.com>
2019-01-21 16:48:56 -0800

    Called api from AlbumList and rendered response (album titles).

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <Text key={album.title}>{album.title}</Text>
+    ));
+  }
+
   render() {
-    return (
-      <View>
-        <Text>Album List!!!</Text>
-      </View>
-    );
+    console.log(this.state);
+
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
